397ba493bfdf21dbe575ec6dfd7892c101c5f296
Drake Song <drakesong216@gmail.com>
2018-03-10 14:36:11 -0800

    Cleaned up code

--- src/FileChecker.java
+++ src/FileChecker.java
@@ -2,14 +2,19 @@ import java.io.*;
 
 public class FileChecker extends javax.swing.filechooser.FileFilter {
 
-    // Checks if the extension of the file is either JPG or PNG
-    protected boolean isImageFile(String ext) {
-        return (ext.equals("jpg") || ext.equals("png"));
-    }
-
     // Returns supported image files
     public String getDescription() {
         return "JPG and PNG images";
+    }
+
+    // Determines if file is a directory or an accepted image file
+    public boolean accept(File f) {
+        return f.isDirectory() || isImageFile(getExtension((f)));
+    }
+
+    // Checks if the extension of the file is either JPG or PNG
+    private boolean isImageFile(String ext) {
+        return (ext.equals("jpg") || ext.equals("png"));
     }
 
     // Returns the extension of the file
@@ -17,13 +22,8 @@ public class FileChecker extends javax.swing.filechooser.FileFilter {
         String s = f.getName();
         int i = s.lastIndexOf('.');
         if (i > 0 && i < s.length() - 1) {
-            return s.substring(i+1).toLowerCase();
+            return s.substring(i + 1).toLowerCase();
         }
         return "";
-    }
-
-    // Determines if file is a directory or an accepted image file
-    public boolean accept(File f) {
-         return f.isDirectory() || isImageFile(getExtension((f)));
     }
 }
